e141653b95a41ee026ccb1b8d62af505e8aefc95
Shane Qi <qizengtai@gmail.com>
2021-07-11 15:04:06 -0500

    Initial Commit

---
+++ Shared/TextStichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextStichesApp.swift
+//  Shared
+//
+//  Created by Shane Qi on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TextStichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
